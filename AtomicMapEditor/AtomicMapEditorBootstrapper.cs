da343ef275211874793148000ab22260745cf67c
Unknown <ken.will.king@gmail.com>
2018-03-25 18:55:29 -0500

    Added unity DI framework and prism framework. Added UnityBootstrapper class.

---
+++ AtomicMapEditor/AtomicMapEditorBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Prism.Unity;
+
+namespace AtomicMapEditor
+{
+    internal class AtomicMapEditorBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+        }
+    }
+}
