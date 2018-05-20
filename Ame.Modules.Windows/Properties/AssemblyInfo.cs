f2004236b29a870764dcb69ec2f58b80a06c181f
Unknown <ken.will.king@gmail.com>
2018-05-20 12:44:32 -0500

    Renamed assembly Ame.Modules.Docks to Ame.Modules.Windows.

--- Ame.Modules.Windows/Properties/AssemblyInfo.cs
+++ Ame.Modules.Windows/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ame.Modules.Docks")]
+[assembly: AssemblyTitle("Ame.Modules.Windows")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ame.Modules.Docks")]
+[assembly: AssemblyProduct("Ame.Modules.Windows")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
